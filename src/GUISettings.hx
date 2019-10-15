af14e6c31049caa337e7495ce500676264f2f3db
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-10-16 00:49:26 +0200

    cleaning

--- src/GUISettings.hx
+++ src/GUISettings.hx
@@ -4,10 +4,10 @@ import model.constants.Quotes;
 
 class GUISettings {
 	public var message:String = 'test';
-	public var speed:Float = 0.8;
-	public var displayOutline:Bool = false;
-	public var noiseStrength:Float = 0.8;
-	public var growthSpeed:Float = 0.8;
+	// public var speed:Float = 0.8;
+	// public var displayOutline:Bool = false;
+	// public var noiseStrength:Float = 0.8;
+	// public var growthSpeed:Float = 0.8;
 	public var maxSize:Float = 0.8;
 	public var fontsize:Float = 40;
 	public var quotes:Array<String> = Quotes.array;
@@ -27,10 +27,10 @@ class GUISettings {
 		trace('update');
 	};
 
-	public var color0 = "#ffae23"; // CSS string
-	public var color1 = [0, 128, 255]; // RGB array
-	public var color2 = [0, 128, 255, 0.3]; // RGB with alpha
-	public var color3 = {h: 350, s: 0.9, v: 0.3}; // Hue, saturation, value
+	// public var color0 = "#ffae23"; // CSS string
+	// public var color1 = [0, 128, 255]; // RGB array
+	// public var color2 = [0, 128, 255, 0.3]; // RGB with alpha
+	// public var color3 = {h: 350, s: 0.9, v: 0.3}; // Hue, saturation, value
 
 	public function new() {}
 }
