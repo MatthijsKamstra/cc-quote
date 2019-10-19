75e619f2256e699622250e11602a771eac424791
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-10-19 11:27:41 +0200

    new color layer

--- src/model/constants/Papertoy.hx
+++ src/model/constants/Papertoy.hx
@@ -6,4 +6,5 @@ class Papertoy {
 	public static var COMBINE_LAYER:String = 'combine_group';
 	public static var TEXT_LAYER:String = 'text_group';
 	public static var CUT_LAYER:String = 'cut_group';
+	public static var COLOR_LAYER:String = 'color_group';
 }
